75be0974a2f97fce4558f2d1e55a5bc29e2e3f7a
hyobeen12 <aszx8683@naver.com>
2024-10-16 12:57:24 +0900

    Add Rocket
    로켓 추가

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -11,10 +11,19 @@ public class Rocket : MonoBehaviour
     void Awake()
     {
         // TODO : Rigidbody2D 컴포넌트를 가져옴(캐싱) 
+        _rb2d = GetComponent<Rigidbody2D>();
     }
-    
+
     public void Shoot()
     {
         // TODO : fuel이 넉넉하면 윗 방향으로 SPEED만큼의 힘으로 점프, 모자라면 무시
+        
+
+        if(fuel > 0)
+        {
+            _rb2d.AddForce(Vector3.up * SPEED, ForceMode2D.Impulse);
+            fuel -= FUELPERSHOOT;
+        }
+        
     }
 }
