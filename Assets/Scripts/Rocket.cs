d0e8a7355f18afd76c37f38280f802267cd22fd0
hyobeen12 <aszx8683@naver.com>
2024-10-18 12:58:39 +0900

    [Add] Rocket score,RetryBtn

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,17 +1,34 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] private Text currentScoreTxt;
+    [SerializeField] private Text BestScoreTxt;
+
     private Rigidbody2D _rb2d;
     private float fuel = 100f;
+
+    Transform RocketTransform;
+    
+
+    float score = 0;
+    
     
     private readonly float SPEED = 5f;
     private readonly float FUELPERSHOOT = 10f;
-    
+
+
     void Awake()
     {
         // TODO : Rigidbody2D 컴포넌트를 가져옴(캐싱) 
         _rb2d = GetComponent<Rigidbody2D>();
+        
+
+        score = gameObject.transform.position.y;
+
     }
 
     public void Shoot()
@@ -25,5 +42,24 @@ public class Rocket : MonoBehaviour
             fuel -= FUELPERSHOOT;
         }
         
+    }
+
+    public void Update()
+    {
+        float y = gameObject.transform.position.y;
+
+        if(score < y)
+        {
+            score = y;
+        }
+
+        currentScoreTxt.text = $"{y.ToString("N2")} M";
+        BestScoreTxt.text = $"HIGH : {score.ToString("N2")} M";
+    }
+
+    public void OnClickRetryBtn()
+    {
+        Destroy(gameObject);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
